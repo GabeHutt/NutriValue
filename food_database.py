14adfe258c53608bb458e6e14a6807fad20b0439
gabehutt <gabehutt@umich.edu>
2024-04-27 18:30:59 -0400

    just to pull

--- food_database.py
+++ food_database.py
@@ -11,10 +11,6 @@ API_SECRET = '4a269d09d9a04870be65fc4d777bcfae'
 
 fs = Fatsecret(API_ID, API_SECRET)
 
-foods = fs.foods_search('rice')
-
-print(foods)
->>>>>>> dfa83a4ede297e2fbb22031dced60bd8c6ba975c
 
 
 
