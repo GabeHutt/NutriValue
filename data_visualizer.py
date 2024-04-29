8819cd9b9150ff590b78526d12060611a2ced84b
gabehutt <gabehutt@umich.edu>
2024-04-29 00:16:23 -0400

    json fixes

--- data_visualizer.py
+++ data_visualizer.py
@@ -7,17 +7,37 @@ import sqlite3
 #rows[int id][hall name id][course_id][meal_id]
 
 def main():
-    enumerate:
-        
-    connect = sqlite3.connect('NutriValue.db')
-    cursor = connect.cursor() 
-    cursor.execute('SELECT * FROM food_references')  
-    rows = cursor.fetchall()
-    for row in rows:
-        hall_name_id = row[1]
-        course_id = row[2]
-        meal_id = row[3]
-        
+    dining_halls = [
+        'place_holder',
+        'East Quad',
+        'Bursely',
+        'South Quad',
+        'Markley',
+        'Mosher Jordan',
+        'Twigs at Oxford',
+        'North Quad'
+    ]
+
+    courses = [
+        'place_holder',
+        'Breakfast',
+        'Brunch',
+        'Dinner'
+    ]
+
+    with sqlite3.connect('NutriValue.db') as connect:
+        cursor = connect.cursor() 
+        cursor.execute('SELECT * FROM food_references')  
+        rows = cursor.fetchall()
+        with sqlite3.connect('NutriValue.db') as nutri_connect:
+            for row in rows:
+                hall_name = dining_halls[row[1]]
+                course = courses[row[2]]
+                meal_id = row[3]
+                nutri_cursor = nutri_connect.cursor()
+                nutri_cursor.execute('SELECT fat_score, carb_score, protein_score FROM food_nutrition WHERE food_name_id =?',(meal_id,))
+
+       
         
 
 
