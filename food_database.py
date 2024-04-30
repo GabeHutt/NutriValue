5296b3deeb4bc225f498e989650201fce09f4453
gabehutt <gabehutt@umich.edu>
2024-04-29 23:03:35 -0400

    secret API

--- food_database.py
+++ food_database.py
@@ -5,9 +5,11 @@ import json
 import re
 from fatsecret import Fatsecret
 
-API_ID = '8ab25fc9a2ef4f6da52799c3ccda208e'
+with open('API.json', 'r') as apis:
+        api_json = json.load(apis)
+API_ID = api_json['API_ID']
 
-API_SECRET = '4a269d09d9a04870be65fc4d777bcfae'
+API_SECRET = api_json['API_SECRET']
 
 fs = Fatsecret(API_ID, API_SECRET)
     
